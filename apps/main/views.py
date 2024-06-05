b1b5e8b68f01b93c728e0e05798ddc5dffc9175f
makksaw <maxim.razumovsky@icloud.com>
2024-06-05 22:33:29 +0300

    First commit

---
+++ apps/main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
+
+def lol(request):
+    return render(request, 'main/lol.html')
+
+def about(request):
+    return render(request, 'main/about.html')
